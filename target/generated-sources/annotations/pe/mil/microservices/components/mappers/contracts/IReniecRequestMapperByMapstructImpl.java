af66c49cacb28ea10809308fa8ea4b3f7027054f
cbaciliodev <dbacilio88@outlook.es>
2023-02-08 00:27:57 -0500

    update project connector web service reniec

--- target/generated-sources/annotations/pe/mil/microservices/components/mappers/contracts/IReniecRequestMapperByMapstructImpl.java
+++ target/generated-sources/annotations/pe/mil/microservices/components/mappers/contracts/IReniecRequestMapperByMapstructImpl.java
@@ -6,8 +6,8 @@ import pe.mil.microservices.dto.requests.ReniecQueryRequest;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-01-13T09:37:33-0500",
-    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 11.0.16 (OpenLogic)"
+    date = "2023-02-08T00:26:22-0500",
+    comments = "version: 1.5.3.Final, compiler: Eclipse JDT (IDE) 3.33.0.v20221215-1352, environment: Java 17.0.5 (Eclipse Adoptium)"
 )
 public class IReniecRequestMapperByMapstructImpl implements IReniecRequestMapperByMapstruct {
 
